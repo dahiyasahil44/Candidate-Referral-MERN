cdf94e233506517560a0ac470e0531e1dca21201
dahiyasahil44 <imsahil440407@gmail.com>
2025-11-20 15:23:15 +0530

    Server hosted on vercel

--- server/server.js
+++ server/server.js
@@ -25,5 +25,5 @@ app.use(function(err, req, res, next) {
   next(err);
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
